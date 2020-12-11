3dfdb62905f79f094f354972266f91f3f0f8032a
Vishal Goel <vishal@wakencode.com>
2020-12-12 01:23:39 +0530

    populating-next-right-pointers-in-each-node-ii  correct soln

--- Solved/Populating Next Right Pointers in Each Node II.cpp
+++ Solved/Populating Next Right Pointers in Each Node II.cpp
@@ -27,7 +27,7 @@ class Node {
       : val(_val), left(_left), right(_right), next(_next) {}
 };
 
-// using BFS
+// using BFS (mine) (with extra space)
 class Solution {
  public:
   Node* connect(Node* root) {
@@ -37,21 +37,100 @@ class Solution {
 
     while (!q.empty()) {
       int size = q.size();
-      Node* prevRight = NULL;
+      Node* prev = NULL;
       while (size--) {
         Node* curr = q.front();
         q.pop();
-        if (curr->left) {
-          if (prevRight) prevRight->next = curr->left;
+        if (curr->left && curr->right) {
+          if (prev) prev->next = curr->left;
           curr->left->next = curr->right;
-          q.push(curr->left);
-        }
-        if (curr->right) {
-          prevRight = curr->right;
           curr->right->next = NULL;
+          prev = curr->right;
+          q.push(curr->left);
+          q.push(curr->right);
+        } else if (curr->left) {
+          if (prev) prev->next = curr->left;
+          curr->left->next = NULL;
+          prev = curr->left;
+          q.push(curr->left);
+
+        } else if (curr->right) {
+          if (prev) prev->next = curr->right;
+          curr->right->next = NULL;
+          prev = curr->right;
           q.push(curr->right);
         }
       }
+    }
+
+    return root;
+  }
+};
+
+// using BFS (with extra space) (above improvised)
+class Solution {
+ public:
+  Node* connect(Node* root) {
+    if (!root) return NULL;
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      int size = q.size();
+      for (int i = 0; i < size; i++) {
+        Node* curr = q.front();
+        q.pop();
+        if (i < size - 1) {
+          curr->next = q.front();
+        }
+
+        if (curr->left) {
+          q.push(curr->left);
+        }
+        if (curr->right) {
+          q.push(curr->right);
+        }
+      }
+    }
+
+    return root;
+  }
+};
+
+// BFS O(1) space
+class Solution {
+ public:
+  Node* connect(Node* root) {
+    if (!root) return NULL;
+    Node* head = nullptr;    // node to traverse next layer in the lower level
+    Node* prev = nullptr;    // previous node in the lower level
+    Node* curr = root;       // current node in the upper level
+
+    while (curr) {
+      while (curr) {
+        if (curr->left) {
+          if (prev) {
+            prev->next = curr->left;
+          } else {
+            head = curr->left;
+          }
+          prev = curr->left;
+        }
+
+        if (curr->right) {
+          if (prev) {
+            prev->next = curr->right;
+          } else {
+            head = curr->right;
+          }
+          prev = curr->right;
+        }
+
+        curr = curr->next;
+      }
+      curr = head;
+      head = nullptr;
+      prev = nullptr;
     }
 
     return root;
